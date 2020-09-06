18c7ae7aaf2947f5a9c50bfa0af281a72bbdffa6
Toxa-p07a1330 <p07a1330@yandex.ru>
2020-09-06 23:00:19 +0300

    with side menu

--- Черновик/frontend/src/App.js
+++ Черновик/frontend/src/App.js
@@ -4,8 +4,7 @@ import Sidemenu from "./Sidemenu";
 
 function get_cookie ( cookie_name )
 {
-    var results = document.cookie.match ( '(^|;) ?' + cookie_name + '=([^;]*)(;|$)' );
-
+    let results = document.cookie.match ( '(^|;) ?' + cookie_name + '=([^;]*)(;|$)' );
     if ( results )
         return ( unescape ( results[2] ) );
     else
@@ -18,18 +17,20 @@ class App extends React.Component{
         this.state = {
             authorized: false,
             login: null,
+            isAdmin:true,
         }
     }
     style = {
         backgroundColor:"#ddd8b1",
         fontFamily: "Courier monospace",
+        minHeight: window.innerHeight,
 
     }
     render(){
         return(
             <div id={"app"} style={this.style}>
                 <Header authorized={get_cookie("login")} login={get_cookie("login")}> </Header>
-                <Sidemenu></Sidemenu>
+                <Sidemenu isAdmin={this.state.isAdmin}></Sidemenu>
             </div>
     )
     };
--- Черновик/frontend/src/Sidemenu.js
+++ Черновик/frontend/src/Sidemenu.js
@@ -1,9 +1,26 @@
 import React from 'react';
 class Sidemenu extends React.Component{
+    style= {
+        borderColor: "#cfc78c",
+        borderStyle: "inset",
+        borderWidth: "5px",
+        display:"inline-block",
+        align: "left",
+        width:"15%",
+        minHeight:"100%"
+    }
     render(props) {
         return (
-            <div>
-
+            <div style={this.style}>
+                <ul>
+                    <li><a>Главная</a></li>
+                    <li><a>Книги</a></li>
+                    <li><a>Авторы</a></li>
+                    <li><a>Отделения библиотеки</a></li>
+                    {this.props.isAdmin?<li><a>Пользователи</a></li>:""}
+                    {this.props.isAdmin?<li><a>История операций</a></li>:""}
+                    {this.props.isAdmin?<li><a>Статистика</a></li>:""}
+                </ul>
             </div>
         );
     }
