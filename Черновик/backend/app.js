870e885594d452ad5a9eae7428dae0d6e57c875b
Toxa-p07a1330 <p07a1330@yandex.ru>
2020-09-08 16:31:31 +0300

    betta backend works

---
+++ Черновик/backend/app.js
@@ -0,0 +1,178 @@
+const http = require("http");
+
+
+
+//DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+
+
+
+
+//DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+
+//todo implement
+class Book{
+    id;
+    name;
+    storageID;
+    authorID;
+    genre;
+    date;
+    isTaken;
+    takersID;
+}
+class User{
+    id;
+    login;
+    password;
+    isAdmin;
+    fName;
+    sName;
+    tName;
+    email;
+    mobilePhone;
+    wasBorn;
+    numberOfBookTaken;
+    isBanned;
+    banReason;
+}
+class Author{
+    id;
+    fName;
+    sName;
+    tName;
+    wasBorn;
+    diedAt;
+}
+class Storage{
+    id;
+    address;
+    startWorkingAt;
+    endWorkingAt;
+}
+class Operation{
+    id;
+    roleRequired;
+    comment;
+}
+class History{
+    id;
+    operationID;
+    userID;
+    bookID;
+    date;
+}
+
+let tables = new Map();
+function initializeMap() {
+    tables.set("user", new User());
+    tables.set("author", new Author());
+    tables.set("storage", new Storage());
+    tables.set("operation", new Operation());
+    tables.set("history", new History());
+    tables.set("book", new Book());
+}
+
+/*
+this block of functions returns JSON representation of object
+@args - string of params in form  key=val&key1=val1
+returns JSON string of rows ejected froms corresponding table with this fields
+*/
+
+
+function sendDataToClient(request, responseTarget){
+    console.log(request);
+    let sqlite3 = require('sqlite3').verbose();
+    let db = new sqlite3.Database('../database/database.db');
+    let promise = new Promise((resolve, reject)=>{
+        db.serialize(()=>{
+            db.all(request, (err, rows)=>{
+                if (err)
+                    reject();
+                else
+                    resolve(rows);
+            })
+        })
+    });
+
+    promise.then(
+        (result)=> {
+        console.log(result);
+        responseTarget.write(JSON.stringify(result));
+        responseTarget.end();
+        db.close();
+    },
+        (error)=>{
+            console.log(error);
+
+        }
+    );
+    return 1;
+}
+function parser(tableName, args, responseTarget){
+
+    let fields = Object.assign({}, tables.get(tableName));
+    convertObjectToArgType(requestParamsToObjest(args), fields)
+    let requestSQL = convertToSQLRequest(fields, tableName);
+    sendDataToClient(requestSQL, responseTarget);
+
+};
+
+
+
+function requestParamsToObjest  (request){
+    let obj = {};
+    let params = request.substr(request.indexOf("?")+1).split("&");
+    if (params.indexOf("favicon")<0){
+        for (let i of params)
+        {
+            let name = i.split("=")[0];
+            let val = i.split("=")[1];
+            obj[name] = val;
+        }
+    }
+    return obj;
+};
+function parceRequest  (requestStr, responseTarget){
+    let parts = requestStr.split("/");
+    if (parts[1]!=="api")
+        return null;
+    else
+        parser(parts[2], parts[3], responseTarget);
+}
+function convertObjectToArgType  (untypedObject, typedObject){
+    for (let i of Object.keys(typedObject))
+        typedObject[i] = untypedObject[i];
+    return typedObject;
+}
+function convertToSQLRequest  (object, tableName){
+let request = "SELECT * FROM "+tableName+" WHERE ";
+    for (let i of Object.keys(object))
+    {
+        if (object[i]!==undefined)
+            request+=i.valueOf()+" = \'"+object[i]+"\' AND ";
+    }
+    request = request.substr(0, request.lastIndexOf("AND"));
+    request+=";"
+    return request;
+}
+
+
+http.createServer(function(request, response){
+
+    request.url = decodeURI(request.url);
+    initializeMap();
+    response.setHeader("Content-Type", "application/json; charset=UTF-8");
+    response.statusCode = 200;
+    if (request.url.indexOf("favicon")>-1)
+        response.end();
+    else {
+        if (request.url.indexOf("api")<0)
+            response.end()
+        else {
+            parceRequest(request.url, response);
+        }
+    }
+
+
+
+}).listen(3001);
